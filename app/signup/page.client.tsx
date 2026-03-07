3c8a4a011a9e513da6221788ff23ab27777ef5d5
czavattiero <czavattiero@gmail.com>
2026-03-07 09:30:16 -0700

    Update page.client.tsx

--- app/signup/page.client.tsx
+++ app/signup/page.client.tsx
@@ -1,33 +1,5 @@
-"use client";
-
-import { Suspense } from "react";
-import { useSearchParams } from "next/navigation";
-
-function SignupContent() {
-  const searchParams = useSearchParams();
-  const plan = searchParams.get("plan");
-
-  return (
-    <div style={{ padding: "40px" }}>
-      <h1>Create your account</h1>
-
-      {plan && (
-        <p>
-          Selected plan: <strong>{plan}</strong>
-        </p>
-      )}
-
-      <p>Signup form goes here.</p>
-    </div>
-  );
-}
+import SignupClient from "./SignupClient";
 
 export default function SignupPage() {
-  return (
-    <Suspense fallback={<div>Loading...</div>}>
-      <SignupContent />
-    </Suspense>
-  );
+  return <SignupClient />;
 }
-
-export const dynamic = "force-dynamic";
