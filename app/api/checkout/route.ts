531762dd1f26f8c70fd6cf67289c9be0b490656b
czavattiero <czavattiero@gmail.com>
2026-03-07 12:11:08 -0700

    Update route.ts

--- app/api/checkout/route.ts
+++ app/api/checkout/route.ts
@@ -1,35 +1,24 @@
-import { NextResponse } from "next/server";
-import Stripe from "stripe";
+import Stripe from "stripe"
 
 const stripe = new Stripe(process.env.STRIPE_SECRET_KEY!, {
-  apiVersion: "2024-06-20"
-});
+  apiVersion: "2024-06-20",
+})
 
-export async function POST(req: Request) {
-
-  const { plan } = await req.json();
-
-  const priceId =
-    plan === "pro"
-      ? process.env.STRIPE_PRO_PRICE_ID
-      : process.env.STRIPE_BASIC_PRICE_ID;
+export async function POST() {
 
   const session = await stripe.checkout.sessions.create({
     mode: "subscription",
-    payment_method_types: ["card"],
 
     line_items: [
       {
-        price: priceId,
-        quantity: 1
-      }
+        price: process.env.STRIPE_BASIC_PRICE_ID!,
+        quantity: 1,
+      },
     ],
 
-    success_url: "http://localhost:3000/dashboard",
-    cancel_url: "http://localhost:3000/"
-  });
+    success_url: "https://web-archive-app.vercel.app/dashboard",
+    cancel_url: "https://web-archive-app.vercel.app/signup",
+  })
 
-  return NextResponse.json({
-    url: session.url
-  });
+  return Response.json({ url: session.url })
 }
