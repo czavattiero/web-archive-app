6e6c00a8f6346cf2444efa02ed81ab5de8051150
czavattiero <czavattiero@gmail.com>
2026-03-07 10:42:48 -0700

    Update page.tsx

--- app/signup/page.tsx
+++ app/signup/page.tsx
@@ -1,6 +1,11 @@
-import SignupClient from "./SignupClient"
+import dynamic from "next/dynamic"
 
 export const dynamic = "force-dynamic"
+
+const SignupClient = dynamic(
+  () => import("./SignupClient"),
+  { ssr: false }
+)
 
 export default function Page() {
   return <SignupClient />
