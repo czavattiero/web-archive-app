d6fafb89d260734e2564122cd9184f519efbf4d1
czavattiero <czavattiero@gmail.com>
2026-03-07 10:03:19 -0700

    Delete app/signup/page.tsx

--- app/signup/page.tsx
+++
@@ -1,9 +0,0 @@
-import dynamic from "next/dynamic";
-
-const SignupClient = dynamic(() => import("./SignupClient"), {
-  ssr: false,
-});
-
-export default function SignupPage() {
-  return <SignupClient />;
-}
