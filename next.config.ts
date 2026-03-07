d40645d3592e2fdc7aee9107fe74533195c047bf
czavattiero <czavattiero@gmail.com>
2026-03-07 09:42:17 -0700

    Disable static prerender checks

--- next.config.ts
+++ next.config.ts
@@ -1,9 +1,16 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
+  output: "standalone",
+
+  experimental: {
+    missingSuspenseWithCSRBailout: false,
+  },
+
   typescript: {
     ignoreBuildErrors: true,
   },
+
   eslint: {
     ignoreDuringBuilds: true,
   },
