192554973dc783a7b0efe6322061c175ff52b021
czavattiero <czavattiero@gmail.com>
2026-03-09 14:14:28 -0600

    Update middleware.ts

--- middleware.ts
+++ middleware.ts
@@ -3,15 +3,10 @@ import type { NextRequest } from "next/server"
 
 export function middleware(req: NextRequest) {
 
-  const token = req.cookies.get("sb-access-token")
-
-  if (!token && req.nextUrl.pathname.startsWith("/dashboard")) {
-    return NextResponse.redirect(new URL("/", req.url))
-  }
-
   return NextResponse.next()
+
 }
 
 export const config = {
-  matcher: ["/dashboard/:path*"]
+  matcher: []
 }
